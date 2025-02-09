d029a1e5cafffc8af7d9e6550e64010f1762a943
unknown <hoai.phat@hotmail.de>
2025-02-09 19:37:56 +0100

    IPSCANNER SKRIPT

---
+++ redteam/c2server/ipscan.bat
@@ -0,0 +1,64 @@
+@echo off
+
+setlocal
+setlocal enabledelayedexpansion
+SET COUNT=1
+for /f "usebackq tokens=2 delims=:" %%a in (`ipconfig ^| findstr /r "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`) do (
+  set _temp=%%a
+  SET var!COUNT!=!_temp:~1!
+  SET /a COUNT=!COUNT!+1
+  )
+SET HOSTIP=%var1%
+SET SUBNETMASK=%var2%
+
+ECHO HOSIP: %HOSTIP%
+ECHO SUBNETMASK: %SUBNETMASK%
+
+::IP-Adresse aufteilen
+set HOSTIP=%HOSTIP:.= % 
+SET COUNT=1
+(for %%a in (%HOSTIP%) do ( 
+   SET i!COUNT!=%%a
+   SET /a COUNT=!COUNT!+1
+))
+
+::SUBNETMASKE AUFTEILEN
+set SUBNETMASK=%SUBNETMASK:.= % 
+SET COUNT=1
+(for %%a in (%SUBNETMASK%) do ( 
+   SET m!COUNT!=%%a
+   SET /a COUNT=!COUNT!+1
+))
+
+::NETWORK
+SET /A "OCTET1= %i1% & %m1%"
+SET /A "OCTET2= %i2% & %m2%"
+SET /A "OCTET3= %i3% & %m3%"
+SET /A "OCTET4= %i4% & %m4%"
+echo NETWORK: %OCTET1%.%OCTET2%.%OCTET3%.%OCTET4%
+
+
+::BROADCAST
+SET /A "OCTET1= %i1% & %m1% | 255-%m1%"
+SET /A "OCTET2= %i2% & %m2% | 255-%m2%"
+SET /A "OCTET3= %i3% & %m3% | 255-%m3%"
+SET /A "OCTET4= %i4% & %m4% | 255-%m4%"
+echo BROADCAST: %OCTET1%.%OCTET2%.%OCTET3%.%OCTET4%
+
+::FIRSTIP
+SET /A "OCTET1= %i1% & %m1%"
+SET /A "OCTET2= %i2% & %m2%"
+SET /A "OCTET3= %i3% & %m3%"
+SET /A "OCTET4= (%i4% & %m4%) + 1"
+echo FIRSTIP: %OCTET1%.%OCTET2%.%OCTET3%.%OCTET4%
+
+::LASTIP
+SET /A "OCTET1= %i1% & %m1% | 255-%m1%"
+SET /A "OCTET2= %i2% & %m2% | 255-%m2%"
+SET /A "OCTET3= %i3% & %m3% | 255-%m3%"
+SET /A "OCTET4= (%i4% & %m4% | 255-%m4%)-1"
+echo LASTIP: %OCTET1%.%OCTET2%.%OCTET3%.%OCTET4%
+
+
+
+endlocal
